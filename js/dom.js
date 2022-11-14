22e727a0601521a11a6124f062c9a32049bdd8c8
guilo3229 <juanpablotorresperezuni@gmail.com>
2022-11-14 12:27:38 +0100

    capitulo 69 Templates HTML

--- js/dom.js
+++ js/dom.js
@@ -1,77 +1,40 @@
-// createTextNode(crea un elemento nodo de texto)
-const $figure = document.createElement("figure"),
-$img= document.createElement("img"),
-$figcaption = document.createElement("figcaption"),
-$figcaptionText = document.createTextNode("Animals"),
-$cards = document.querySelector(".cards")
-// aqui ya dinamicamente estan creadas esas etiquetas pero no estan incorporadas en el arbol del DOM y hay que ir agregandolo
 
-// Antes de ejecutar el appendchild  al metodo cards tendria que configurar dichos elementos: a la figura agregar la imagen y la figcaption y a la ficaption el nodo texto:
 
-// Tambien en la etiqueta img vamos a necesitar emterle sus atributos(src...etc) para ello
-$img.setAttribute("src", "https://placeimg.com/200/200/animals")
-$img.setAttribute("alt", "Animals")
-$figcaption.appendChild($figcaptionText)
-$figure.appendChild($img)
-// al nodo figure necesitamos agregarle la clase card para que quede como card
-$figure.classList.add("card")
-$figure.appendChild($figcaption)
-$cards.appendChild($figure)
 
-// Otra forma de hacerlo:
-const $figure2 = document.createElement("figure")
-// usaremos la propieudad innerHTML para agregarle las cosas
-$figure2.innerHTML = `
-<img src="https://placeimg.com/200/200/people" alt = "People">
-<figcaption>People</figcaption>
-`;
-$figure2.classList.add("card")
-$cards.appendChild($figure2);
+const $cards = document.querySelector(".cards")
+const $template = document.getElementById("template-card").content
+const $fragment = document.createDocumentFragment(),
+cardContent =[
+    {
+        title:"Tecnologia",
+        img: "https://placeimg.com/200/200/tech"
+    },
+    {
+        title:"Animales",
+        img: "https://placeimg.com/200/200/animals"
+    },
+    {
+        title:"Arquitectura",
+        img: "https://placeimg.com/200/200/arh"
+    },
+    {
+        title:"Gente",
+        img: "https://placeimg.com/200/200/people"
+    },
+    {
+        title:"Naturaleza",
+        img: "https://placeimg.com/200/200/nature"
+    }
+];
 
-// que pasa si tenemos varios elementos que queremos agregar:
-const estaciones = ["Primavera","Verano","Otoño","invierno"],
-$ul = document.createElement("ul")
-
-document.write("<h3>Estaciones del Año</h3>")
-document.body.appendChild($ul)
-// En esto va creando un li por cada recorrido que ahce el ForEach de nuestro array no los machaca uno encima del otro
-estaciones.forEach(el =>{
-    const $li = document.createElement("li")
-    $li.textContent = el
-    $ul.appendChild($li)
+cardContent.forEach(el =>{
+    $template.querySelector("img").setAttribute("src",el.img)
+    
+    $template.querySelector("img").setAttribute("alt",el.title)
+    
+    $template.querySelector("figcaption").textContent = el.title;
+    // Ahora clonariamos dicha estructura para que no haya problemas, ya que ese template es unico digamos que si yo usaria ese template para la primera tarjeta no estaria dispobible para la 2 da 3cera EventCounts, lo que tenemos que hacer es clonarlo,el true dice que copie toda la estructura no solo la etiqueta de open y de cierre
+    let $clone = document.importNode($template,true)
+    $fragment.appendChild($clone)
 })
-// ahora lo haremos con HTML
-const continentes = ["África","VerAmérica","Asia","Europa","Oceanía "],
-$ul2 = document.createElement("ul")
-document.body.appendChild($ul2);
-
-document.write("<h3>Continendel Mundo</h3>")
-document.body.appendChild($ul2)
-$ul2.innerHTML = ""
-// PAra que no lo machaque tenemos que hacerle el += por eso tenemos que hacerla vacia explicita, para que no de error aunque eso era entes
-continentes.forEach(el=>($ul2.innerHTML += `<li>${el}</li>`))
-
-const meses = [
-"Enero",
-"Febrero",
-"MArzo",
-"Abril",
-"Mayo",
-"Junio",
-"Julio",
-"Agosto",
-"Septiembre",
-"Octubre",
-"Noviembre",
-"Diciembre"
-],
-$ul3 = document.createElement("ul"),
-$fragment = document.createDocumentFragment()
-meses.forEach(el => {
-    const $li = document.createElement("li");
-    $li.textContent =el;
-    $fragment.appendChild($li)
-})
-document.write("<h3>Meses del Año</h3>")
-$ul3.appendChild($fragment)
-document.body.appendChild($ul3)
+$cards.appendChild($fragment)
