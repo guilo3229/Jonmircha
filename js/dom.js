e3548528c49d54ce37a6cd4499d3523830e28416
guilo3229 <juanpablotorresperezuni@gmail.com>
2022-11-14 10:09:24 +0100

    capitulo 67  recorriendo el dom

--- js/dom.js
+++ js/dom.js
@@ -1,23 +1,37 @@
+// Esta enfocado a los elementos, son nodos tipo elementos
 
-// Lo que vamos hacer primeramente es capturar la variable de un elemento del HTML
+const $cards = document.querySelector(".cards")
+console.log($cards)
+// Para ver sus hijos en la consola ponemos.children y nos devuelve un HTML
+console.log($cards.children)
+console.log($cards.children[2])
+// Childnodes hace referencia a los nodos hijos, podemos ver que en la lista de nodos un text entre cada figure.card nodo ya que esos text son los saltos de lineas espacios)
+console.log($cards.childNodes)
 
-const $whatIsDom = document.getElementById("que-es")
+// con .parent te muestra el elemento siguiente padre y si le pones el .parentnode tambien es el body porque es el nodo padre
+console.log($cards.parentNode)
+console.log($cards.parentElement)
+// firtChild hace referencia al primer hijo nodo
+console.log($cards.firstChild)
+// para hace referencia al primer elemento hijo:
+console.log($cards.firstElementChild)
+// para detectar el ultimo nodo y detectar el ultimo elemento hijo:
+console.log($cards.lastChild)
+console.log($cards.lastElementChild)
+// Tambien podemos buscar el hermano que esta antes y el hermano que esta despues, su hermano anterior seria el enlace y su hermano posterior la seccion script.
+// lo mismo qcon nodo y elemento:
+console.log($cards.previousSibling)
+console.log($cards.previousElementSibling)
+// El siguiente hermano
+console.log($cards.nextSibling)
+console.log($cards.nextElementSibling)
 
-let text = `<p>
-El Modelo de Objetos del Documento(<b><i>DOM - Document Object Model</i></b>) es un API para documentos HTML y XML
-</p>
-<p>
-Este proveé una representación estructural del documento, permitiendo modificar su contenido y presentacion visual mediante codigo JS.
-</p>
-<p>
-El DOM no es parte de la especificacion de javsScript, es una API para los navegadores
-</p>
-`;
-// iinet text no  identifica las etiquetas porque es antigua
-// $whatIsDom.innerText = text
-// text Content tampoco identifica las etiquetas
-$whatIsDom.textContent = text
-$whatIsDom.innerHTML = text
-// Lo que ahce outer HTML es remplazar el contenido del DOM con el parrafo que esta de manera independiente no lo mete en un parrafo y dentro los otros 3 parrafos
-$whatIsDom.outerHTML = text
+console.log($cards.previousSibling)
+// Un nuevo metodo es el metodo closes, va a buscar el padre mas cercano del tipo de selector que nosotros le demos
+console.log($cards.closest("div"))
+console.log($cards.closest("body"))
+console.log($cards.children[3].closest("section"))
+
+
+
 
