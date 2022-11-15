85f9f9633576fcb43f8066f1b315d2dade209650
guilo3229 <juanpablotorresperezuni@gmail.com>
2022-11-15 21:12:05 +0100

    capitulo 71 Modificando elmentos (COOL STYLE)

--- js/dom.js
+++ js/dom.js
@@ -1,19 +1,29 @@
 
 
+
+// .inserAdjacent
+// .inserAdjacentElement(position, el)
+// .insertAdjacentHTML(position, html)
+// .insertAdjacentText(position,text)
+
+// Posiciones:
+// Lo podrian antes de ese parrafo de ese nodo que pongas de referencia
+    // beforebegin(hermano anterior)
+    // afterbegin(primer hijo)
+    // beforeend(ultimo hijo)
+    // afterend(hermano siguiente)
+
+
 const $cards = document.querySelector(".cards"),
     $newCard = document.createElement("figure");
-    $cloneCards =$cards.cloneNode(true)
-$newCard.innerHTML = `
-<img src="https://placeimg.com/200/200/any" alt="Any">
-<figcaption>Any</figcaption>
-`
 
-$newCard.classList.add("card")
-// replace child sirve para sustituir
-// $cards.replaceChild($newCard, $cards.children[2])
-// hay un metodo para insertar antes de un nodo, no susituye simplemente lo pone antes del nodo indicado, puedes ahcerlo con first elmentchild o poeer el [0] con cards.children tambien funcionarioa]
-// $cards.insertBefore($newCard,$cards.firstElementChild)
-// Tambien tenemos un emtodo para eliminar
-// $cards.removeChild($cards.lastElementChild)
-// El Dom tiene un metodo que nos permite clonar para estos elmentos serian cloneNode, es un poco diferente al importclone del HTML
-document.body.appendChild($cloneCards)
+    let $contentCard = `
+    <img src ="https://placeimg.com/200/200/any"alt="Any">
+    <figcaption></figcaption>
+    `;
+    $newCard.classList.add("card");
+    $newCard.insertAdjacentHTML("beforeend",$contentCard)
+    $newCard.querySelector("figcaption").insertAdjacentText("afterbegin","Any")
+    $cards.insertAdjacentElement("afterbegin", $newCard)
+
+    
