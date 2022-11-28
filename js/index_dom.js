a6df00f8eb84df5f6db280495ddd7622d16b3d4d
guilo3229 <juanpablotorresperezuni@gmail.com>
2022-11-28 01:15:45 +0100

    capitulo 81 completo

---
+++ js/index_dom.js
@@ -0,0 +1,10 @@
+import hamburguerMenu from "./dom/menu_hamburguesa.js";
+
+const d = document;
+d.addEventListener("DOMContentLoaded",e=>{
+
+
+    hamburguerMenu(".panel-btn",".panel",".menu a")
+
+
+})
