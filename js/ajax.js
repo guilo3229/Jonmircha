c3bdc41b111e4e22c682ca3720f96c6e002736e6
guilo3229 <juanpablotorresperezuni@gmail.com>
2023-01-05 12:08:25 +0100

    108 AJAX:API Fetch +Async-await  paso de try a ctach con throw mismamente dentro de la promesa asyncrona y no como return en una funcion externa

--- js/ajax.js
+++ js/ajax.js
@@ -1,4 +1,4 @@
-// Crearemos una funcion anonima auto-ejecutable porque en este mismo archivo nos va a enseñar a trabjar con fetch y axios y para evitar sobreescritura mejor en una funcion anonima autoejecutable enc ada ejercicio para clausurarlo en cada uno, sin necesidad de modulos de momento, de esta manera haremos todos los ejercicios en un unico archivo. las funciones autoejecutables era lo que se usaba antes, pero con la aparicion de modulos ha ido un poco en desuso
+// Crearemos una funcion anonima auto-ejecutable porque en este mismo archivo nos va a enseñar a trabjar con fetch y axios y para evitar sobreescritura mejor en una funcion anonima autoejecutable en cada ejercicio para clausurarlo en cada uno, sin necesidad de modulos de momento, de esta manera haremos todos los ejercicios en un unico archivo. las funciones autoejecutables era lo que se usaba antes, pero con la aparicion de modulos ha ido un poco en desuso
 
 (()=>{
     //1º- Paso numero uno la instancia
@@ -43,6 +43,7 @@
     $fragment = document.createDocumentFragment();
 
 // recuerda que fetch trabaja con promesas, then para  opciones cuando la promesa se cumple y catch para la parte negativa cuando la promesa no se cumple, el finally es par afinalizarr y siempre va a estar se cumpla o no la promesa.
+// Recuerda el metodo por defecto para fetch es GET!! es algo deiferente que la forma de encima
     fetch("https://jsonplaceholder.typicode.com/users")
     
      // Fetch tambien funciona localmente
@@ -51,7 +52,7 @@
 
     .then(res=>{
                // La informacion viene en el body pero es una readableStream, esto hay que convertirlo con el metodo necesario en este caso es json(.json,.text y .block lo podemos verlo en mozilla developer network)
-        console.log(res)
+        // console.log(res)
        return res.ok
     //    Aqui le hacemos un operador ternario, esto es porque la promsesa se summple es decir se envia y vuelve pero sin datos pero la accion la hace, ara que no vulva y salga reject hay que meterse en la propiedad de la promesa y aplicar su reject cuando una propiedad que vuelva no nos guste y vaya directamente al catch que seria el error, este no se ejecutara si vuelve la promesa y esta no sea satisfactoria
        ? res.json()
@@ -67,12 +68,48 @@
         $fetch.appendChild($fragment)
         // Fetch tambien funciona localmente
        
-        console.log(json)
+        // console.log(json)
     })
     .catch(err=> {
         let message = err.statusText || "Ocurrio un error";
         $fetch.innerHTML = `Error ${err.status}: ${message} `
-    }).finally(()=> console.log("esto se ejecutara independientemente de la promesa fetch"))
+    }).finally(()=>{
+    // console.log("esto se ejecutara independientemente de la promesa fetch")
+})
     
 
-})()
+})();
+
+(() =>{
+    const $fetchAsync = document.getElementById("fetch-async"),
+    $fragment = document.createDocumentFragment();
+async function getData(){
+    try{
+        
+        let res = await fetch("https://jsonplaceholder.typicode.com/user"),
+        json = await res.json();
+        console.log(res, json);
+        // el throw es un return que envia el flujo de la programacion al catch, el error no puede mandar objetos solo texto
+        // if(!res.ok)throw new Error("Ocurrio un Error al solicitar los DAtos")
+        // Por ello podemos hacer un objeto que marque los errores:
+        if(!res.ok)throw{status:res.status,statusTExt: res.statusText}
+        json.forEach(el =>{
+            const $li =document.createElement("li")
+            $li.innerHTML =`${el.name} -- ${el.email} -- ${el.phone} `
+            $fragment.appendChild($li)
+        })
+        $fetchAsync.appendChild($fragment)
+    }catch (err){
+        console.log("Estoy en el catch", err)
+        // le mandamos el objeto al catch cuando es erroneo la solicitud y dicho objeto podemos usarlo luego para aplicar sus propeidades que nos interesa ene l DOM
+        let message = err.statusText || "Ocurrio un error";
+        $fetchAsync.innerHTML = `Error ${err.status}: ${message} `
+    }finally{
+        console.log("esto se ejecutara independientemente del try-catch")
+    }
+    // fetch("https://jsonplaceholder.typicode.com/users")
+    
+
+}
+    getData();
+})();
