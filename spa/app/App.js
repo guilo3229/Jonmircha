04a4a759d8a155248fb27692874e2b449ddde228
guilo3229 <juanpablotorresperezuni@gmail.com>
2023-02-05 01:54:29 +0100

    capitulo 149 SPA

--- spa/app/App.js
+++ spa/app/App.js
@@ -1,17 +1,15 @@
 import api from "./helpers/wp_api.js"
 import {ajax} from "./helpers/ajax.js"
-import { Title } from "./components/PostCard.js"
+
 import { Loader } from "./components/Loader.js"
+import { Header } from "./components/Header.js"
 export function App (){
     const d = document,
     // creamos el nodo para meterle el elemnto que viene desde la aplicacion PostCArd
     $root = d.getElementById("root")
-    $root.appendChild(Title())
+    $root.appendChild(Header())
     $root.appendChild(Loader())
 
-    ajax({
-        url:"no-valida",
-        cbsuccess:()=>{}
-    })
+
 }
 
---
+++ spa/app/components/SearchForm.js
@@ -0,0 +1,15 @@
+export function SearchForm(){
+    const $form = document.createElement("form"),
+    $input = document.createElement("input")
+    // el input por default es un input de tipo texto
+    $form.classList.add("form-search")
+    // vamos a darle un nombre a ese input par apoder acceder a su progrmacion posteriormente
+    $input.name = "search"
+    // El typo search no spone la x para borrar todo ta bueno
+    $input.type = "search"
+    $input.placeholder = "Buscar..."
+    $form.appendChild($input)
+
+
+    return $form
+}
